82ff97a6f32553e4ceb41d6cc4b11d37cff379dc
Julia-slvbrg <julia.slvb@gmail.com>
2023-10-30 21:54:22 -0300

    feat: structure for the single page app defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [client, setClient] = useState([])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <Modal /> */}
+      <h1>Quem faz parte da mesa?</h1>
+      <form>
+        <input
+          id="name"
+          name="name"
+          placeholder="Nome"
+          type="text"
+        />
+        
+        <button>Adicionar</button>
+
+      </form>
+
+      <button>Adicione um valor para ser divido</button> {/* this button calls a modal to add the value to be divided */}
+
+      <> {/* container to show the clientes names and what each has consumed */} </>
+      
     </>
   )
 }
